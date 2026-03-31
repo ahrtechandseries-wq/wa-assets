d0ad452a59a331ec397076edbe29eee9c450ea70
ahrtechandseries-wq <ahrtechandseries@gmail.com>
2026-03-31 18:50:29 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,13 @@
+// Service Worker Install Event
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+// Service Worker Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Baki Workbox code nicher moto thakbe...
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-
-if (workbox) {
-  // HTML ba Page gulo age net theke anar try korbe, na pele cache theke dibe (Offline Support)
-  workbox.routing.registerRoute(
-    ({request}) => request.mode === 'navigate',
-    new workbox.strategies.NetworkFirst({
-      cacheName: 'nexflix-pages',
-    })
-  );
-
-  // CSS, JS ar Font cache korbe
-  workbox.routing.registerRoute(
-    ({request}) =>
-      request.destination === 'style' ||
-      request.destination === 'script' ||
-      request.destination === 'font',
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'nexflix-assets',
-    })
-  );
-
-  // Image gulo cache korbe jate data kom kate ar fast load hoy
-  workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst({
-      cacheName: 'nexflix-images',
-      plugins: [
-        new workbox.expiration.ExpirationPlugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    })
-  );
-}
+// ... (Apnar baki code)
