4391c3cabb0c0268b907d440e956a1cdb25b542b
ahrtechandseries-wq <ahrtechandseries@gmail.com>
2026-03-31 20:30:09 +0600

    Refactor service worker event listeners and add fetch
    Updated event listener syntax and added fetch event handling.

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-// Service Worker Install Event
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
 
-// Service Worker Activate Event
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (e) => {
+  return self.clients.claim();
 });
 
-// Baki Workbox code nicher moto thakbe...
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-// ... (Apnar baki code)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
