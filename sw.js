2c5b7cd9dd13de1574a570480d6a9e710ddd12ab
ahrtechandseries-wq <ahrtechandseries@gmail.com>
2026-03-31 18:01:25 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,36 @@
-const CACHE_NAME = 'nexflix-cache-v2';
-const urlsToCache = ['/', 'https://r-nexflixx.blogspot.com'];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+if (workbox) {
+  // HTML ba Page gulo age net theke anar try korbe, na pele cache theke dibe (Offline Support)
+  workbox.routing.registerRoute(
+    ({request}) => request.mode === 'navigate',
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'nexflix-pages',
+    })
   );
-});
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+  // CSS, JS ar Font cache korbe
+  workbox.routing.registerRoute(
+    ({request}) =>
+      request.destination === 'style' ||
+      request.destination === 'script' ||
+      request.destination === 'font',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'nexflix-assets',
+    })
   );
-});
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+  // Image gulo cache korbe jate data kom kate ar fast load hoy
+  workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst({
+      cacheName: 'nexflix-images',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    })
   );
-});
-                       
+}
