86f5846c3c9738c3ed7a47f5b8a9d60ce6741387
ahrtechandseries-wq <ahrtechandseries@gmail.com>
2026-05-28 09:00:08 +0600

    Add caching and fetch support in sw.js
    Implement service worker caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,4 +1,41 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+const CACHE_NAME = "nexflix-cache-v1";
 
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => console.log('NexFlix PWA Active'));
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "https://i.ibb.co/ycbgxNjg/50a5e599037e.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch (offline support + speed boost)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate (clean old cache)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
